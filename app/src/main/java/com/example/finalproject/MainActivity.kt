41e0cbc79b750cdcb88ec2af5df4e68fe515921f
Alexander Rakusevich <124079950+TransmisionesFerox@users.noreply.github.com>
2024-05-16 00:36:56 +0500

    aa

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -9,6 +9,7 @@ import com.example.finalproject.fragment.RecipeListFragment
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var searchView: SearchView
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -21,6 +22,25 @@ class MainActivity : AppCompatActivity() {
             .commit()
 
     }
+//private val sharedPrefs by lazy {
+    //getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
+    //}
+    //override fun onCreate(savedInstanceState: Bundle?) {
+        //super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        // Сохранение значений
+        //val editor = sharedPrefs.edit()
+        //editor.putString("username", "Anupam")
+        //editor.putLong("l", 100L)
+        //editor.apply()
+
+        // Получение значений
+        //val username = sharedPrefs.getString("username", "defaultName")
+        //val lValue = sharedPrefs.getLong("l", 1L)
+
+        // Используйте полученные значения в вашем приложении
+        // Например, отобразите имя пользователя или другие данные
+    }
 
 
-}
